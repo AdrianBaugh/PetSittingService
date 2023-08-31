09f6f70c7cfa80f3436df19a2bec0c20a180805e
Justin Locke <Justindeanlocke@gmail.com>
2023-08-31 16:08:29 -0500

    Functional first page draft

--- web/src/pages/test.js
+++ web/src/pages/test.js
@@ -1,5 +1,42 @@
-const main = async () => {
-    console.log('Future home of river Pet sitting!!');
-};
+import BindingClass from "../util/bindingClass";
 
+export default class Header extends BindingClass {
+    constructor() {
+        super();
+
+        const methodsToBind = [
+            'addHeaderToPage', 'createSiteTitle'
+        ];
+        this.bindClassMethods(methodsToBind, this);
+    }
+
+
+
+async addHeaderToPage() {
+    const currentUser = await this.client.getIdentity();
+
+    const siteTitle = this.createSiteTitle();
+    const userInfo = this.createUserInfoForHeader(currentUser);
+
+    const header = document.getElementById('header');
+    header.appendChild(siteTitle);
+    header.appendChild(userInfo);
+}
+
+createSiteTitle() {
+    const homeButton = document.createElement('a');
+    homeButton.classList.add('header_home');
+    homeButton.href = 'index.html';
+    homeButton.innerText = 'Playlists';
+
+    const siteTitle = document.createElement('div');
+    siteTitle.classList.add('site-title');
+    siteTitle.appendChild(homeButton);
+
+    return siteTitle;
+}
+}
+const main = async () => {
+    console.log('I created a new page!');
+};
 window.addEventListener('DOMContentLoaded', main);
