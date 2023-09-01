9f4f066a2ca7c29a9edf344fa5cfac6c2008b40b
Yanan <Evazhao919@gmail.com>
2023-09-01 13:26:59 -0500

    make a GetPetRequestClass with out tests

---
+++ MusicPlaylistServiceLambda/src/main/java/com/nashss/se/musicplaylistservice/activity/requests/GetPetRequest.java
@@ -0,0 +1,40 @@
+package com.nashss.se.musicplaylistservice.activity.requests;
+
+    public class GetPetRequest {
+        private final String petId;
+
+        public GetPetRequest(String petId) {
+            this.petId = petId;
+        }
+
+        public String getPetId() {
+            return petId;
+        }
+
+        @Override
+        public String toString() {
+            return "GetPetRequest{" +
+                    "petId='" + petId + '\'' +
+                    '}';
+        }
+        //CHECKSTYLE:OFF:Builder
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public static class Builder {
+            private String petId;
+
+            public Builder withPetId(String petId) {
+                this.petId = petId;
+                return this;
+            }
+
+            public GetPetRequest build() {
+                return new GetPetRequest(petId);
+            }
+        }
+    }
+
+
+
