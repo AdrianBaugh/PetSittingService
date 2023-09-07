9cf681acb208ae1b4fd890bf04845c8a5e31d370
Yanan <Evazhao919@gmail.com>
2023-09-06 21:42:21 -0500

    add the test

--- MusicPlaylistServiceLambda/src/test/java/com/nashss/se/musicplaylistservice/activity/GetReservationActivityTest.java
+++ MusicPlaylistServiceLambda/src/test/java/com/nashss/se/musicplaylistservice/activity/GetReservationActivityTest.java
@@ -1,23 +1,45 @@
-//package com.nashss.se.musicplaylistservice.activity;
-//
-//import com.nashss.se.musicplaylistservice.activity.requests.GetPlaylistRequest;
-//import com.nashss.se.musicplaylistservice.activity.results.GetPlaylistResult;
-//import com.nashss.se.musicplaylistservice.dynamodb.PlaylistDao;
-//import com.nashss.se.musicplaylistservice.dynamodb.ReservationDao;
-//import com.nashss.se.musicplaylistservice.dynamodb.models.Reservation;
-//
-//import com.google.common.collect.Sets;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mock;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.Mockito.when;
-//import static org.mockito.MockitoAnnotations.initMocks;
-//
-//public class GetReservationActivityTest {
-//
-//    }
-//}
+package com.nashss.se.musicplaylistservice.activity;
+
+import com.nashss.se.musicplaylistservice.activity.requests.GetReservationRequest;
+import com.nashss.se.musicplaylistservice.activity.results.GetReservationResponse;
+import com.nashss.se.musicplaylistservice.dynamodb.ReservationDao;
+import com.nashss.se.musicplaylistservice.dynamodb.models.Reservation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class GetReservationActivityTest {
+    @Mock
+    private ReservationDao reservationDao;
+    private GetReservationActivity getReservationActivity;
+
+    @BeforeEach
+    public void setUp() {
+        initMocks(this);
+        getReservationActivity = new GetReservationActivity(reservationDao);
+    }
+
+    @Test
+    public void handleRequest_withValidFields_getReservationSuccess() {
+        // GIVEN
+        String reservationId = "666666";
+        Reservation reservation = new Reservation();
+        reservation.setReservationId(reservationId);
+
+        //WHEN
+        when(reservationDao.getReservationById(reservationId)).thenReturn(reservation);
+
+        GetReservationRequest reservationRequest = GetReservationRequest.builder()
+                .withReservationId(reservationId)
+                .build();
+        GetReservationResponse reservationResponse = getReservationActivity.handleRequest(reservationRequest);
+
+        //THEN
+        assertEquals(reservationId,reservationResponse.getReservation().getReservationId());
+    }
+}
